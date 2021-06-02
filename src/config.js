9371ef9f1aa93a0a9e908fe19c13ac914d557603
Shehan Prasanna <shehanp068@gmail.com>
2021-06-02 14:40:17 +0530

    frontend

---
+++ src/config.js
@@ -0,0 +1,20 @@
+const config = {
+  
+    s3: {
+      REGION: "ap-south-1",
+      BUCKET: "notes-api-prod-serverlessdeploymentbucket-1dedyda5s1idw",
+      IDENTITY_POOL_ID: "ap-south-1:b79df574-afee-4556-9785-d1adeb7606d7"
+    },
+    apiGateway: {
+      REGION: "ap-south-1",
+      URL: "https://anzcdryphe.execute-api.ap-south-1.amazonaws.com/prod/",
+    },
+    cognito: {
+      REGION: "ap-south-1",
+      USER_POOL_ID: "ap-south-1_NmptlLWJM",
+      APP_CLIENT_ID: "40kcvcuacq8ntvsnta027b1ts1",
+      IDENTITY_POOL_ID: "ap-south-1:b79df574-afee-4556-9785-d1adeb7606d7",
+    },
+  };
+  
+  export default config;
